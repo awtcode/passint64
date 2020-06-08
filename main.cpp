55eb588f100f8e7c392d12e02fe8fd0b9151fa92
Kevin Cheung <kevin.cheung@autodesk.com>
2020-06-08 12:13:07 +0800

    Add more testcases

--- main.cpp
+++ main.cpp
@@ -3,15 +3,27 @@
 
 extern int64_t fnLongLong(int arg, int arg1);
 
+int64_t (*longFnType)(int arg, int arg1);
+
 EMSCRIPTEN_KEEPALIVE
 int main(int argc, char *argv[]) {
     printf("main+\n");
     int64_t ret = 0;
     try {
       ret = fnLongLong(argc, 0);
+      printf("invoke:%lld\n", ret);
     } catch (...) {
       printf("exception caught!!");
     }
 
+    ret = fnLongLong(argc, 0);
+    printf("call_direct:%lld\n", ret);
+
+    longFnType = nullptr;
+    if (argc == 0) {
+      longFnType = &fnLongLong;
+      ret = longFnType(argc, 0);
+      printf("fnptr:%lld\n", ret);
+    }
     return ret;
 }
